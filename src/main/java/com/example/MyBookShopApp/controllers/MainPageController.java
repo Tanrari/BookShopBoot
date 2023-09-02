fc4a6ad55df7078b0fa19958b991b08f04bf9996
Tanrari <tanrari@junior.com>
2023-09-02 18:34:30 +0300

    1

--- src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -1,6 +1,9 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,11 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/bookshop")
 public class MainPageController {
 
-    @GetMapping("/main")
-    public String mainPage(){
-        System.out.println("1");
-        return "index";
+    private final BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
     }
+
+//    @GetMapping("/main")
+//    public String mainPage(){
+//        System.out.println("1");
+//        return "index";
+//    }
 
     @GetMapping("/genres")
     public String genres(){
@@ -24,4 +34,14 @@ public class MainPageController {
     public String authors(){
         return "../authors/index";
     }
+
+
+    @GetMapping("/main")
+    public String mainPage(Model model){
+        model.addAttribute("bookData",bookService.getBooksData());
+
+        return "index";
+
+    }
+
 }
