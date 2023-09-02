06fd34fa2fb71940785e005efcb744d7b3b3e380
Tanrari <tanrari@junior.com>
2023-09-02 10:03:57 +0300

    1

---
+++ src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -0,0 +1,27 @@
+package com.example.MyBookShopApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bookshop")
+public class MainPageController {
+
+    @GetMapping("/main")
+    public String mainPage(){
+        System.out.println("1");
+        return "index";
+    }
+
+    @GetMapping("/genres")
+    public String genres(){
+        System.out.println("2");
+        return "genres";
+    }
+
+    @GetMapping("/authors")
+    public String authors(){
+        return "author";
+    }
+}
